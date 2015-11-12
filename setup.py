3fc442563694dbb5d395fe045aa6c1c566b60de1
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-12 00:57:59 -0500

    setup / requirement updates

--- setup.py
+++ setup.py
@@ -15,8 +15,13 @@ setup(
     ],
     packages=find_packages(),
     entry_points={
-        'ene_irc.plugins': ['datetime = ene_irc.plugins.datetime:Plugin']
+        'ene_irc.plugins': [
+            'datetime = ene_irc.plugins.datetime:DateTime',
+            'logging = ene_irc.plugins.logging:Logger',
+            'test = ene_irc.plugins.test:Test'
+        ]
     },
     install_requires=['twisted>=15.4.0,<15.5', 'sqlalchemy>=1.0.9,<1.1', 'alembic>=0.8.3,<0.9', 'pymysql>=0.6.7,<0.7',
-                      'voluptuous>=0.8.7,<0.9', 'appdirs>=1.4.0,<1.5', 'agentml>=0.3.1,<0.4', 'venusian>=1.0,<1.1']
+                      'voluptuous>=0.8.7,<0.9', 'appdirs>=1.4.0,<1.5', 'agentml>=0.3.1,<0.4', 'venusian>=1.0,<1.1',
+                      'ircmessage>=0.1,<0.2']
 )
