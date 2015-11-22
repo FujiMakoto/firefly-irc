4a6ee066fb83905ff364f91062b1661b9133d14f
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-21 21:42:15 -0500

    Test notice routing

--- tests/test_eneirc.py
+++ tests/test_eneirc.py
@@ -167,6 +167,32 @@ class PluginEventTestCase(EneIRCTestCase):
         mock_channelMessage.assert_not_called()
         mock__fire_command.assert_not_called()
 
+    @mock.patch.object(EneIRC, '_fire_event')
+    @mock.patch.object(EneIRC, '_fire_command')
+    @mock.patch.object(EneIRC, 'channelNotice')
+    @mock.patch.object(EneIRC, 'privateNotice')
+    def test_channel_notice_routed(self, mock_privateNotice, mock_channelNotice, mock__fire_command,
+                                    mock__fire_event):
+        ene = EneIRC(Server(self.hostname, self.config))
+        ene.noticed('test_nick!~user@example.org', '#testchan', 'Hello, world!')
+
+        self.assertEqual(mock_channelNotice.call_count, 1)
+        mock_privateNotice.assert_not_called()
+        mock__fire_command.assert_not_called()
+
+    @mock.patch.object(EneIRC, '_fire_event')
+    @mock.patch.object(EneIRC, '_fire_command')
+    @mock.patch.object(EneIRC, 'channelNotice')
+    @mock.patch.object(EneIRC, 'privateNotice')
+    def test_private_notice_routed(self, mock_privateNotice, mock_channelNotice, mock__fire_command,
+                                    mock__fire_event):
+        ene = EneIRC(Server(self.hostname, self.config))
+        ene.noticed('test_nick!~user@example.org', 'test_nick', 'Hello, world!')
+
+        self.assertEqual(mock_privateNotice.call_count, 1)
+        mock_channelNotice.assert_not_called()
+        mock__fire_command.assert_not_called()
+
 
 class PluginCommandTestCase(EneIRCTestCase):
 
