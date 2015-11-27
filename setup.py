b43a4833e979a9d5f4ed55c4121ea32796a5dc3b
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-27 05:03:38 -0500

    setup.py and requirements updates

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
+import os
 from setuptools import setup, find_packages
+
+
+def find_data_files():
+    data_files = []
+
+    for root, dirs, files in os.walk('plugins'):
+        for name in files:
+            if name.endswith('.cfg') or name.endswith('.aml'):
+                data_files.append(os.path.join(root, name))
+
+    for root, dirs, files in os.walk('config'):
+        for name in files:
+            if name.endswith('.cfg'):
+                data_files.append(os.path.join(root, name))
+
+    return data_files
+
 
 setup(
     name='Firefly IRC',
@@ -24,11 +42,11 @@ setup(
             'test = firefly.plugins.test:Test'
         ]
     },
-    install_requires=['twisted>=15.4.0,<15.5', 'sqlalchemy>=1.0.9,<1.1', 'alembic>=0.8.3,<0.9', 'pymysql>=0.6.7,<0.7',
+    install_requires=['twisted>=15.4.0,<15.5', 'click>=6.0,<6.1', 'alembic>=0.8.3,<0.9', 'pymysql>=0.6.7,<0.7',
                       'voluptuous>=0.8.7,<0.9', 'appdirs>=1.4.0,<1.5', 'agentml>=0.3.1,<0.4', 'venusian>=1.0,<1.1',
                       'ircmessage>=0.1,<0.2', 'arrow>=0.7,<0.8'],
     test_requires=['mock>=1.3,<1.4', 'coveralls>=1.1,<1.2'],
     package_data={
-        'firefly': ['config/*.cfg'],
+        'firefly': find_data_files(),
     },
 )
